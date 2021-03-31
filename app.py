4e8c583011b060a05fefc3d9cc29772ba586d7d2
Gozde Onal <ggozdeonal@gmail.com>
2021-03-31 19:36:30 +0300

    /api/users/<user_id>/homes endpoint is added

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-userdb = {}
+# default entry
+userdb = {'aa': {'name': 'baby', 'surname': 'yoda', 'password': 'ss', 'phone': '999888777'}}
 
 @app.route('/api/users', methods=['POST'])
 @cross_origin()
@@ -37,6 +38,27 @@ def login():
             return jsonify({'token': 'fake-jwt-token', "status":"SUCCESS","message":"login is successful","user":{"user_id":"2","name":"Işıl","surname":"Güneş","email":"igunes@gmail.com","phone":"+905435586504"},"session":{"start_time":"Thu Mar 18 2021 22:09:41 GMT+0300 (GMT+03:00)","expire_time":"Fri Mar 19 2021 10:09:41 GMT+0300 (GMT+03:00)","session_key":"admin"}}), 200
         return jsonify({"status":"SUCCESS","message":"login has failed"}), 400
     return jsonify({"status":"SUCCESS","message":"login has failed"}), 400
+
+
+@app.route('/api/users/<user_id>/homes', methods=['POST'])
+@cross_origin()
+def addHome(user_id):
+    if request.method == 'POST':
+        home_data = request.get_json()
+
+        print(home_data)
+        home_name = home_data.get('home_name')
+        isVisible = home_data.get('isVisible')
+        country = home_data.get('country')
+        state = home_data.get('state')
+        city = home_data.get('city')
+        neighbourhood = home_data.get('neighbourhood')
+        latitude = home_data.get('latitude')
+        longitude = home_data.get('longitude')
+
+
+        return jsonify({"status":"SUCCESS","message":"new home is added","user_id":"1","home":{"home_id":"2","home_owner":"1","home_name":"İst 1","isVisible":False,"country":"Turkey","state":"Sinop","city":"Merkez","neighbourhood":"X Mah. Y Cad. Z Sok. No: 2/4","latitude":"42.2548","longitude":"34.5897"}}), 200
+    return jsonify({"status":"FAILURE","message":"request body fields are not true"}), 400
 
 
 if __name__ == '__main__':
