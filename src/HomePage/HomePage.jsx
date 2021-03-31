f577228139135d115d09c198016d823c4bad1361
Gozde Onal <ggozdeonal@gmail.com>
2021-03-31 20:21:57 +0300

    Home addition page is updated

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -32,7 +32,7 @@ function HomePage() {
         e.preventDefault();
 
         setSubmitted(true);
-        if (home.homeName && home.isVisible && home.country && home.state && home.city && home.neighbourhood && home.latitude && home.longitude) {
+        if (home.homeName && home.country && home.state && home.city && home.neighbourhood && home.latitude && home.longitude) {
             dispatch(userActions.addHome(home));
         }
     }
@@ -40,83 +40,76 @@ function HomePage() {
     return (
         <div className="col-lg-8 offset-lg-2">
             <div className="card bg-light">
-                <article className="card-body col-lg-6 offset-lg-3">
+                <article className="card-body col-lg-8 offset-lg-2">
                     <h4 className="card-title mt-3 text-center">Ev Ekle</h4>
                     <p className="text-center">Yeni bir ev ekleyin</p>
                     <p className="divider-text"></p>
+                    <br/>
                     <form name="addHomeForm" onSubmit={handleAddHome}>
-                        <div className="form-group input-group">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text"> <i className="fa fa-envelope"></i> </span>
-                            </div>
-                            <input type="text" name="homeName" placeholder="Ev Adi" value={home.homeName} onChange={handleChange} className={'form-control' + (submitted && !home.homeName ? ' is-invalid' : '')} />
+                        <div className="form-group">
+                            <label htmlFor="exampleFormControlTextarea1">Ev Adi</label>
+                            <input type="text" name="homeName" placeholder="Sogutozu Ev" value={home.homeName} onChange={handleChange} className={'form-control' + (submitted && !home.homeName ? ' is-invalid' : '')} />
                             {submitted && !home.homeName &&
                             <div className="invalid-feedback">Email is required</div>
                             }
                         </div>
-                        <div className="form-group input-group">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
-                            </div>
-                            <input type="text" name="isVisible" placeholder="Herkese Gorunur" value={home.isVisible} onChange={handleChange} className={'form-control' + (submitted && !home.isVisible ? ' is-invalid' : '')} />
-                            {submitted && !home.isVisible &&
-                            <div className="invalid-feedback">Password is required</div>
-                            }
-                        </div>
-                        <div className="form-group input-group">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
-                            </div>
+
+                        <div className="form-group">
+                            <label htmlFor="exampleFormControlTextarea1">Ulke</label>
                             <input type="text" name="country" placeholder="Ulke" value={home.country} onChange={handleChange} className={'form-control' + (submitted && !home.country ? ' is-invalid' : '')} />
                             {submitted && !home.country &&
                             <div className="invalid-feedback">Password is required</div>
                             }
                         </div>
-                        <div className="form-group input-group">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
-                            </div>
-                            <input type="text" name="state" placeholder="Il" value={home.state} onChange={handleChange} className={'form-control' + (submitted && !home.state ? ' is-invalid' : '')} />
+
+                        <div className="form-group">
+                            <label htmlFor="exampleFormControlTextarea1">Il</label>
+                            <input type="text" name="state" placeholder="Ankara" value={home.state} onChange={handleChange} className={'form-control' + (submitted && !home.state ? ' is-invalid' : '')} />
                             {submitted && !home.state &&
                             <div className="invalid-feedback">Password is required</div>
                             }
                         </div>
-                        <div className="form-group input-group">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
-                            </div>
-                            <input type="text" name="city" placeholder="Ilce" value={home.city} onChange={handleChange} className={'form-control' + (submitted && !home.city ? ' is-invalid' : '')} />
+
+                        <div className="form-group">
+                            <label htmlFor="exampleFormControlTextarea1">Ilce</label>
+                            <input type="text" name="city" placeholder="Cankaya" value={home.city} onChange={handleChange} className={'form-control' + (submitted && !home.city ? ' is-invalid' : '')} />
                             {submitted && !home.city &&
                             <div className="invalid-feedback">Password is required</div>
                             }
                         </div>
-                        <div className="form-group input-group">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
-                            </div>
-                            <input type="text" name="neighbourhood" placeholder="Mahalle / Acik Adres" value={home.neighbourhood} onChange={handleChange} className={'form-control' + (submitted && !home.neighbourhood ? ' is-invalid' : '')} />
+
+                        <div className="form-group">
+                            <label htmlFor="exampleFormControlTextarea1">Acik Adres</label>
+                            <textarea rows="3" type="text" name="neighbourhood" placeholder="Söğütözü Caddesi No:43, 06560" value={home.neighbourhood} onChange={handleChange} className={'form-control' + (submitted && !home.neighbourhood ? ' is-invalid' : '')} />
                             {submitted && !home.neighbourhood &&
                             <div className="invalid-feedback">Password is required</div>
                             }
                         </div>
-                        <div className="form-group input-group">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
+
+                        <div className="form-row row justify-content-between">
+                            <div className="form-group col-md-5">
+                                <label htmlFor="exampleFormControlTextarea1">Enlem</label>
+                                <input type="text" name="latitude" placeholder="39.859155" value={home.latitude} onChange={handleChange} className={'form-control' + (submitted && !home.latitude ? ' is-invalid' : '')} />
+                                {submitted && !home.latitude &&
+                                <div className="invalid-feedback">Password is required</div>
+                                }
                             </div>
-                            <input type="text" name="latitude" placeholder="Enlem" value={home.latitude} onChange={handleChange} className={'form-control' + (submitted && !home.latitude ? ' is-invalid' : '')} />
-                            {submitted && !home.latitude &&
-                            <div className="invalid-feedback">Password is required</div>
-                            }
-                        </div>
-                        <div className="form-group input-group">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
+                            <div className="form-group col-md-5">
+                                <label htmlFor="exampleFormControlTextarea1">Boylam</label>
+                                <input type="text" name="longitude" placeholder="32.931109" value={home.longitude} onChange={handleChange} className={'form-control' + (submitted && !home.longitude ? ' is-invalid' : '')} />
+                                {submitted && !home.longitude &&
+                                <div className="invalid-feedback">Password is required</div>
+                                }
                             </div>
-                            <input type="text" name="longitude" placeholder="Boylam" value={home.longitude} onChange={handleChange} className={'form-control' + (submitted && !home.longitude ? ' is-invalid' : '')} />
-                            {submitted && !home.longitude &&
-                            <div className="invalid-feedback">Password is required</div>
-                            }
                         </div>
+
+                        <br/>
+                        <div className="custom-control custom-switch">
+                            <input type="checkbox" className="custom-control-input" id="customSwitch1" name="isVisible" value={home.isVisible} onChange={handleChange}/>
+                            <label className="custom-control-label" htmlFor="customSwitch1">Ev Herkese Gorunsun</label>
+                        </div>
+                        <br/>
+
                         <div className="form-group">
                             <button type="submit" className="btn btn-primary btn-block">Temizle</button>
                             <button type="submit" className="btn btn-warning btn-block">Evi Kaydet</button>
