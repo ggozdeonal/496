372480efad6ee40ecf39ea0639d0e7147eb12a5a
Nilay Yorgancılar <nyorgancilar@etu.edu.tr>
2021-04-12 21:17:22 +0300

    Event info dialog is added.

--- src/components/Table/TableEvent.js
+++ src/components/Table/TableEvent.js
@@ -12,16 +12,53 @@ import IconButton from "@material-ui/core/IconButton";
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import InfoIcon from '@material-ui/icons/Info';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Tooltip from "@material-ui/core/Tooltip";
-
+import Button from "components/CustomButtons/Button.js";
 const useStyles = makeStyles(styles);
 
 export default function CustomTable(props) {
   const classes = useStyles();
   const { tableHead, tableData, tableHeaderColor } = props;
+  const [open, setOpen] = React.useState(false);
+
+  const handleCloseInfo = () => {
+    setOpen(false);
+
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <div className={classes.tableResponsive}>
- 
+     <Dialog  open={open} onClose={handleCloseInfo} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title3">Etkinlik Bilgileri</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Katılımcı Sayısı: 3
+              
+            </DialogContentText>
+            <DialogContentText>
+              Katılımcılar: sfhaf sfakjhf asfhaf
+              
+            </DialogContentText>
+            
+  
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseInfo} color="danger">
+              Kapat
+            </Button>
+      
+            
+          </DialogActions>
+      </Dialog>
+
       <Table className={classes.table}>
         {tableHead !== undefined ? (
           <TableHead className={classes[tableHeaderColor + "TableHeader"]}>
@@ -73,7 +110,7 @@ export default function CustomTable(props) {
         title="Etkinlik Bilgileri"
         placement="top"
         classes={{tooltip:classes.tooltip}}>
-  <IconButton aria-label="InfoIcon" className={classes.tableActionButton}>
+  <IconButton aria-label="InfoIcon" onClick={handleClickOpen} className={classes.tableActionButton}>
                   <InfoIcon className={classes.tableActionButtonIcon + " " + classes.edit}/>
                 </IconButton>
       </Tooltip>
