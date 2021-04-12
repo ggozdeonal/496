54403d16d22d6842dc536ca8378da1d7c03b0c1a
ggozdeonal <ggozdeonal@gmail.com>
2021-04-12 15:56:55 +0300

    Update LoginPage.jsx
    Login with Google updated

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -9,6 +9,34 @@ import loginStyle from "assets/css/bauphi.css";
 import { history } from '../_helpers';
 import { alertActions } from '../_actions';
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyANQzjmvAokWtm7N_NbgqVIx6rPTXyCD1w",
+    authDomain: "bauphi-a9ccf.firebaseapp.com",
+    projectId: "bauphi-a9ccf",
+    storageBucket: "bauphi-a9ccf.appspot.com",
+    messagingSenderId: "66954904557",
+    appId: "1:66954904557:web:42ce6cf7ce5f2697599de4",
+    measurementId: "G-JYDNCV3DNR"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
+var provider = new firebase.auth.GoogleAuthProvider();
+
+
 function LoginPage() {
     const [inputs, setInputs] = useState({
         email: '',
@@ -32,7 +60,6 @@ function LoginPage() {
 
     function handleChange(e) {
         const { name, value } = e.target;
-
         setInputs(inputs => ({ ...inputs, [name]: value }));
     }
 
@@ -49,12 +76,34 @@ function LoginPage() {
 
     function loginWithGoogle()
     {
-        console.log("Google ile giris", email, password);
+        firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+
+            console.log(token + "..." + user.email);
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
     }
 
     function loginWithFacebook()
     {
-        console.log("Facebook ile giris", email, password);
+        console.log("Facebook ile giris");
     }
 
     return (
