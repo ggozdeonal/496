d688436370d7dce9c841441b9b41a42d4d91f5f4
tunahanertekin <tertekin@etu.edu.tr>
2021-04-17 15:16:19 +0300

    deployment config is added

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
+            apiUrl: 'http://localhost:' + (process.env.PORT || '4000') 
         })
     }
 }
