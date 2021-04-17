ec3c2a3285b4bc5e18efb56703ea9e9b745cbeaf
tunahanertekin <tertekin@etu.edu.tr>
2021-04-17 14:51:39 +0300

    event participation is removed

--- src/components/Table/TableEvent.js
+++ src/components/Table/TableEvent.js
@@ -19,56 +19,34 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Tooltip from "@material-ui/core/Tooltip";
 import Button from "components/CustomButtons/Button.js";
+import {useDispatch, useSelector} from "react-redux";
+import {userActions} from "../../_actions";
+import TextField from '@material-ui/core/TextField';
+
+
 const useStyles = makeStyles(styles);
 
 export default function CustomTable(props) {
   const classes = useStyles();
-  const { tableHead, tableData, tableHeaderColor } = props;
-  const [open, setOpen] = React.useState(false);
-  
-  const handleCloseInfo = () => {
-    setOpen(false);
+  const { tableHead, tableData, tableHeaderColor, events } = props;
 
-  };
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
 
   return (
     <div className={classes.tableResponsive}>
-     <Dialog  open={open} onClose={handleCloseInfo} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title3">Etkinlik Bilgileri</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Katılımcı Sayısı: 3
-              
-            </DialogContentText>
-            <DialogContentText>
-              Katılımcılar: sfhaf sfakjhf asfhaf
-              
-            </DialogContentText>
-            
-  
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCloseInfo} color="danger">
-              Kapat
-            </Button>
       
-            
-          </DialogActions>
-      </Dialog>
+
+
+
+
+
+
+
+     
 
       <Table className={classes.table}>
         {tableHead !== undefined ? (
           <TableHead className={classes[tableHeaderColor + "TableHeader"]}>
             <TableRow className={classes.tableHeadRow}>
-            <TableCell
-                    className={classes.tableCell + " " + classes.tableHeadCell}
-                    key={0}
-                  >
-                    Aksiyon
-            </TableCell>
               {tableHead.map((prop, key) => {console.log(key+1);
                 return (
                   <TableCell
@@ -86,37 +64,8 @@ export default function CustomTable(props) {
           {tableData.map((prop, key) => {
             return (
               <TableRow key={key} className={classes.tableBodyRow}>
-              <TableCell className={classes.tableCell} key={0}>
-              <Tooltip
-        id="tooltip-katil"
-        title="Etkinliğe Katıl"
-        placement="top"
-        classes={{tooltip:classes.tooltip}}>
-     <IconButton aria-label="ThumbUpIcon" className={classes.tableActionButton}>
-                  <ThumbUpIcon className={classes.tableActionButtonIcon + " " + classes.edit}/>
-                </IconButton>
-      </Tooltip>
-      <Tooltip
-        id="tooltip-ayril"
-        title="Etkinlikten Ayrıl"
-        placement="top"
-        classes={{tooltip:classes.tooltip}}>
-    <IconButton aria-label="ThumbDownIcon" className={classes.tableActionButton}>
-                  <ThumbDownIcon className={classes.tableActionButtonIcon + " " + classes.edit}/>
-                </IconButton>
-      </Tooltip>
-      <Tooltip
-        id="tooltip-bilgi"
-        title="Etkinlik Bilgileri"
-        placement="top"
-        classes={{tooltip:classes.tooltip}}>
-  <IconButton aria-label="InfoIcon" onClick={handleClickOpen} className={classes.tableActionButton}>
-                  <InfoIcon className={classes.tableActionButtonIcon + " " + classes.edit}/>
-                </IconButton>
-      </Tooltip>
-                
-                
-              </TableCell>
+              
+
                 {prop.map((prop, key) => {
                   return (
                     <TableCell className={classes.tableCell} key={key+1}>
